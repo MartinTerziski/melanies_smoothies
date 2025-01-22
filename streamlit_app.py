109a91cf1dfbfcd8937c0120846f2be9316b73ba
MartinTerziski <martinterziski@yahoo.com>
2025-01-22 14:48:47 +0000

    Added logic for naming the order

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,9 @@ st.write(
     """
 )
 
+name_on_order = st.text_input('Name on Smoothie:')
+st.write('The name on your Smoothie will be:', name_on_order)
+
 cnx = st.connection("snowflake")
 session = cnx.session()
 
@@ -29,13 +32,14 @@ if ingredients_list:
 
     #st.write(ingredients_string)
     
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
-            values ('""" + ingredients_string + """')"""
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+            values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
 
-    #st.write(my_insert_stmt)
+    st.write(my_insert_stmt)
+    
     time_to_insert=st.button('Submit order')
     
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         
-        st.success('Your Smoothie is ordered!', icon="✅")
+        st.success('Your Smoothie is ordered ' + name_on_order + '!', icon="✅")
